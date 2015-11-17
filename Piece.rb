1961485ce53a33c523956db3a0815cff379de490
gajohnso <gajohnso@mtu.edu>
2015-11-17 16:56:29 -0500

    Initial framework for the Piece class

---
+++ Piece.rb
@@ -0,0 +1,61 @@
+require 'gosu'
+
+#require other libraries as needed
+
+#Piece classs, establishes and tracks piece properties
+
+class Piece
+  #create piece properties
+  @@owner
+  @@position = Array.new(2)
+  @@king = false
+  @@captured = false
+  
+  #constructor
+  def new(posx=0, posy=0)
+    #set piece position
+    @@position[0] = posx
+    @@position[1] = posy
+  end
+  
+  #change owner of the piece
+  def set_owner(new_owner)
+    @@owner = new_owner
+  end
+  
+  #retrieve the owner of the piece
+  def get_owner()
+    return @@owner
+  end
+  
+  #retrieve the position of the piece
+  def get_pos()
+    return @@position
+  end
+  
+  #change the position of the piece
+  def set_pos(posx, posy)
+    @@position[0] = posx
+    @@position[1] = posy
+  end
+  
+  #retrieve the king status
+  def is_king()
+    return @@king
+  end
+  
+  #change the king status
+  def change_king(king_status)
+    @@king = king_status
+  end
+  
+  #retrieve the capture status
+  def is_captured()
+    return @@captured
+  end
+  
+  #change the capture status
+  def change_capture(capture_status)
+    @@capture = capture_status
+  end
+end
