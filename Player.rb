8db71b9d9298f69afb38742932209f2086c162f5
Gabriel Revells <gcrevell@mtu.edu>
2015-12-06 13:36:28 -0500

    Filled make_move and valid_move? methods.

--- Player.rb
+++ Player.rb
@@ -17,16 +17,50 @@ class Player
 
 	#move a piece to a location, or return an error(?)
 	def make_move(piece, location)
-		
+		if valid_move?(piece, location)
+			piece.positon = location
+		else
+			return nil
+		end
 	end
 
 	#return true if the move can be made
 	def valid_move?(piece, location)
+		if piece.get_owner() == @color
+			return false
+		end
 		
+		if @board.piece_at(location) != nil
+			return false
+		end
+		
+		if (@color == "red") && (location.y > piece.positon.y) && (piece.king == false)
+			return false
+		elsif (location.y < piece.positon.y) && (piece.king == false)
+			return false
+		end
+		
+		for i in 1..2
+			if (location.x + i == piece.positon.x) && (location.y + i == piece.positon.x)
+				return true
+			end
+			
+			if (location.x - i == piece.positon.x) && (location.y + i == piece.positon.x)
+				return true
+			end
+			
+			if (location.x + i == piece.positon.x) && (location.y - i == piece.positon.x)
+				return true
+			end
+			
+			if (location.x - i == piece.positon.x) && (location.y - i == piece.positon.x)
+				return true
+			end
+		end
 	end
 
 	#get the list of pieces in your color
 	def my_pieces()
-		return @board.get_pieces_by_color(@color)
+		return @board.get_pieces_by_owner(@color)
 	end
 end
